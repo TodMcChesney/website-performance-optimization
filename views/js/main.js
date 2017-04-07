dde0ad1930c67d6ace5481712a71979bc51d603e
TodMcChesney <codescribe.net@gmail.com>
2017-04-07 17:08:39 +0200

    refactor: Update pizza creation loop
    Used a document fragment inside the for loop to append each pizza to.
    Then once all of the pizzas are created the fragment gets appended to
    the page. This results in better performance.

--- views/js/main.js
+++ views/js/main.js
@@ -450,11 +450,14 @@ var resizePizzas = function(size) {
 
 window.performance.mark('mark_start_generating'); // collect timing data
 
-// This for-loop actually creates and appends all of the pizzas when the page loads
+// This for-loop actually creates all of the pizzas when the page loads
+// Once they are all created they are appended to the page
 var pizzasDiv = document.getElementById('randomPizzas');
+var fragment = document.createDocumentFragment();
 for (var i = 2; i < 100; i++) {
-  pizzasDiv.appendChild(pizzaElementGenerator(i));
+  fragment.appendChild(pizzaElementGenerator(i));
 }
+pizzasDiv.appendChild(fragment);
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark('mark_end_generating');
